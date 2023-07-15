9153b23ce9f223a52207d9e2fcae16c29bd435a2
MeguminAl <117918758+MeguminAl@users.noreply.github.com>
2023-07-15 19:13:27 +0300

    main.js

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// Cart
 let cartIcon = document.querySelector("#cart-icon");
 let cart = document.querySelector(".cart");
 let closeCart = document.querySelector("#close-cart");
@@ -11,12 +12,11 @@ cartIcon.onclick = () => {
 }
 
 
-if (document.readyState() == "loading") {
+if (document.readyState == "loading") {
     document.addEventListener("DOMContentLoaded", ready);
 } else {
     ready();
 }
-
 
 function ready() {
     var removeCartButtons = document.getElementsByClassName("cart-remove");
@@ -25,13 +25,35 @@ function ready() {
         var button = removeCartButtons[i];
         button.addEventListener("click", removeCartItem);
     }
+    // Quantity Changes
     var quntityInputs = document.getElementsByClassName("cart-quantity");
     for (var i = 0; i < quntityInputs.length; i++) {
         var input = quntityInputs[i];
         input.addEventListener("change", quntityChanged);
     }
+    // Add To Cart
+    var addCart = document.getElementsByClassName("add-cart");
+    for (var i = 0; i < addCart.length; i++) {
+        var button = addCart[i];
+        button.addEventListener("click", addCartClicked);
+    }
+    // Buy Button Work
+    document
+    .getElementsByClassName("btn-buy")[0]
+    .addEventListener("click", buyButtonClicked);
 }
 
+// Buy Button 
+function buyButtonClicked(){
+    alert("You Order is placed");
+    var cartContent = document.getElementsByClassName("cart-content")[0]
+    while (cartContent.hasChildNodes()){
+        cartContent.removeChild(cartContent.firstChild);
+    }
+    updatetotal();
+}
+
+// Remove Items From Cart
 function removeCartItem(event) {
     var buttonClicked = event.target;
     buttonClicked.parentElement.remove();
@@ -46,6 +68,44 @@ function quntityChanged(event) {
     updatetotal();
 }
 
+function addCartClicked(event) {
+    var button = event.target;
+    var shopProducts = button.parentElement;
+    var title = shopProducts.getElementsByClassName("product-title")[0].innerText;
+    var price = shopProducts.getElementsByClassName("price")[0].innerText;
+    var productImg = shopProducts.getElementsByClassName("product-img")[0].src;
+    addProductToCart(title, price, productImg);
+    updatetotal();
+}
+
+function addProductToCart(title, price, productImg){
+    var cartShopBox = document.createElement("div");
+    cartShopBox.classList.add("cart-box");
+    var cartItems = document.getElementsByClassName("cart-content")[0];
+    var cartItemsNames = cartItems.getElementsByClassName("cart-product-title");
+    for (var i = 0; i < cartItemsNames.length; i++){
+        alert("you have already add this item to cart");
+        return;
+    }
+}
+
+var cartBoxContent = `
+                        <img src="${productImg}" alt="" class="cart-img">
+                        <div class="detail-box">
+                            <div class="cart-product-title">${title}</div>
+                            <div class="cart-price">${price}</div>
+                            <input type="number" value="1" class="cart-quantity">
+
+                            <i class="bx bxs-trash-alt cart-remove"></i>`;
+cartShopBox.innerHTML = cartBoxContent;
+cartItems.append(cartShopBox);
+cartShopBox
+    .getElementsByClassName("cart-remove")[0]
+    .addEventListener("click", removeCartItem);
+cartShopBox
+    .getElementsByClassName("cart-quantity")[0]
+    .addEventListener("change", quntityChanged);
+
 function updatetotal() {
     var cartContent = document.getElementsByClassName("cart-content")[0];
     var cartBoxes = cartContent.getElementsByClassName("cart-box");
@@ -57,9 +117,10 @@ function updatetotal() {
         var price = parseFloat(priceElement.innerText.replace("$", ""));
         var quantity = quantityElement.value;
         total= total + (price * quantity);
+    }
+        // If price Contain some Cents Value
         total = Math.round(total * 100) / 100;
 
         document.getElementsByClassName("total-price")[0].innerText = "$" + total;
-    }
 }
 
